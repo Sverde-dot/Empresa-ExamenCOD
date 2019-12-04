9f29a13e6d9d8ec91fba00083e4c96a9ac0de994
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2019-12-04 14:11:06 +0100

    Update Aparcamiento.kt

--- Aparcamiento.kt
+++ Aparcamiento.kt
@@ -3,13 +3,13 @@ data class Aparcamiento (
 //Ahora crearemos las variables nombre, plazas y plazas libre.
 //Estas se hacen con var (nombre de la variable): tipo ="datos"
     var nombre: String = "Aparcamiento falso123"){
-
     var plazas: Int= 123
     var plibres: Int = 100
 //Para acabar hacemos un metodo de nombre init en el que marcamos los datos del garage
     init {
+//Primero pondre para que se muestre en pantalla algo como "El garage" aunque su funcion sea inutil        
         println("El garage...")
-
+//y en este caso voy a marcar que (nombre) contenga ademas (plazas) y (plibres)
         nombre = nombre +" "+plazas+" "+plibres
     }
 }
